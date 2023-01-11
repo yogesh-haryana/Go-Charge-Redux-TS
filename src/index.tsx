bcfee0608bc9a1a6d6cb2fe9e80b24d9b8b38357
yogesh-haryana <personalyogesh352@gmail.com>
2023-01-11 22:25:51 +0530

    typescript issue solved

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,12 @@ import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
 );
 
 reportWebVitals();
--- src/redux/reducers/reducers.ts
+++ src/redux/reducers/reducers.ts
@@ -14,6 +14,7 @@ export interface StationObject {
 }
 
 export interface StateType  {
+  [x: string]: any;
   stationsData: StationObject[],
 }
 
