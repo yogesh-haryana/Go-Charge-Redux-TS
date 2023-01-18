3af1f8a3d2506b11b7daef3a918a0e674e4690b0
yogesh-haryana <personalyogesh352@gmail.com>
2023-01-18 14:42:47 +0530

    constant location changed to dynamic

--- src/components/StationsInfo.tsx
+++ src/components/StationsInfo.tsx
@@ -1,21 +1,29 @@
 import useStyles from "./StationInfo";
 import Logo from "../assets/logoHead.png";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { StateType, StationObject } from "../redux/reducers/reducers";
 import ClipLoader from "react-spinners/ClipLoader";
 
 const StationsInfo: React.FC = () => {
   const classes = useStyles();
+  const [myLatitude, setMyLatitude] = useState(0);
+  const [myLangitude, setMyLongitude] = useState(0);
 
-  const myLoc = {
-    // my location
-    lat: 28.588265,
-    long: 77.313712,
-  };
-  
+  //  getting current location(latitude and longitude)
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((posn) => {
+      setMyLatitude(posn.coords.latitude);
+      setMyLongitude(posn.coords.longitude);
+    });
+  }, [myLangitude, myLangitude]);
+ 
   //   getting data from the store
-  const { stationsData, isStationsLoading } = useSelector((state: StateType) => state.GetStations);
-  
+  const { stationsData, isStationsLoading } = useSelector(
+    (state: StateType) => state.GetStations,
+  );
+
   function calculateDistance( //function to calulate distance between geo locations
     lat1: number,
     lon1: number,
@@ -42,34 +50,38 @@ const StationsInfo: React.FC = () => {
 
   return (
     <div className={classes.stationsHolder}>
-      { !isStationsLoading && (stationsData.length > 0) && stationsData.map((item: StationObject, i:number) => {
-        return (
-          <div className={classes.stationClass} key={i}>
-            <div className={classes.logoContainer}>
-              <img src={Logo} alt="logo"></img>
+      {!isStationsLoading &&
+        stationsData.length > 0 &&
+        stationsData.map((item: StationObject, i: number) => {
+          return (
+            <div className={classes.stationClass} key={i}>
+              <div className={classes.logoContainer}>
+                <img src={Logo} alt="logo"></img>
+              </div>
+              <div className={classes.stationDetails}>
+                <p className={classes.stationName}>{item?.stationName}</p>
+                <p>
+                  distance -
+                  {calculateDistance(
+                    myLatitude,
+                    myLangitude,
+                    item?.location?.lat,
+                    item?.location?.lang,
+                  )}{" "}
+                  kms.
+                </p>
+                <p className={classes.address}>Address - {item?.address}</p>
+                <p>
+                  <>Charging Rate - Rs. {item?.ratesPerHour} /hr.</>
+                </p>
+              </div>
             </div>
-            <div className={classes.stationDetails}>
-              <p className={classes.stationName}>{item?.stationName}</p>
-              <p>
-                distance -
-                {calculateDistance(
-                  myLoc.lat,
-                  myLoc.long,
-                  item?.location?.lat,
-                  item?.location?.lang,
-                )}{" "}
-                kms.
-              </p>
-              <p className={classes.address}>Address - {item?.address}</p>
-              <p>
-                <>Charging Rate - Rs. {item?.ratesPerHour} /hr.</>
-              </p>
-            </div>
-          </div>
-        );
-      })}
-      { !isStationsLoading && stationsData.length === 0 && <span>No Matching Results Available</span>}
-      { isStationsLoading && <ClipLoader color="#61876E" size={50} /> }
+          );
+        })}
+      {!isStationsLoading && stationsData.length === 0 && (
+        <span>No Matching Results Available</span>
+      )}
+      {isStationsLoading && <ClipLoader color="#61876E" size={50} />}
     </div>
   );
 };
