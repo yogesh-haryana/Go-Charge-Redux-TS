e15fef3e08d4479948edd7ba42b647732fb72cfc
yogesh-haryana <personalyogesh352@gmail.com>
2023-01-23 11:46:43 +0530

    react versions and loading setTimeout removed

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,20 +3,26 @@ import axios from "axios";
 import SearchIcon from "@mui/icons-material/Search";
 import Header from "./Header";
 import { useDispatch, useSelector } from "react-redux";
-import { searchStation, chnageInputValue, setStationLoder, updateMessage } from "../redux/actions/action";
+import {
+  searchStation,
+  chnageInputValue,
+  setStationLoder,
+  updateMessage,
+} from "../redux/actions/action";
 import { StateType } from "../redux/reducers/reducers";
 
 const SearchBar = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
-  const { inputValue } = useSelector((state: StateType)=> state.GetStations)
-    
+  const { inputValue } = useSelector((state: StateType) => state.GetStations);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(chnageInputValue(e.target.value));
   };
 
   const fetchData = async () => {
-    const resp = await axios.get(`https://gocharge.onrender.com/api/allStations/${inputValue}`);
+    const resp = await axios.get(
+      `https://gocharge.onrender.com/api/allStations/${inputValue}`);
     const { data } = resp;
     dispatch(searchStation(data));
     dispatch(setStationLoder(false));
@@ -24,19 +30,19 @@ const SearchBar = () => {
   };
 
   const postSearchHistory = async () => {
-    const data = { searchQuery: inputValue }
-    const resp = await axios.post("https://gocharge.onrender.com/api/allStations/history", data);
+    const data = { searchQuery: inputValue };
+    const resp = await axios.post(
+      "https://gocharge.onrender.com/api/allStations/history",
+      data);
     console.log("post data is ", resp);
-  }
+  };
 
   const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     dispatch(setStationLoder(true));
-    setTimeout(() => {
-      fetchData();
-    }, 500);
+    fetchData();
     postSearchHistory();
     e.preventDefault();
-  }
+  };
 
   return (
     <div>
@@ -45,7 +51,7 @@ const SearchBar = () => {
         Search a charging station for Your Electric Vehicle
       </p>
       <div className={classes.searchBarContainer}>
-        <form onSubmit={(e)=>onFormSubmit(e)}>
+        <form onSubmit={(e) => onFormSubmit(e)}>
           <input
             className={classes.searchBar}
             type="text"
@@ -53,10 +59,7 @@ const SearchBar = () => {
             onChange={(e) => handleChange(e)}
             value={inputValue}
           ></input>
-          <button
-            type="submit"
-            className={classes.searchButton}
-          >
+          <button type="submit" className={classes.searchButton}>
             <SearchIcon />
           </button>
         </form>
